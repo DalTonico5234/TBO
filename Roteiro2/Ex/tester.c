9fa4545d7979f80cb8912a326ea06120cad8c639
Leonardo Roxo <lcoroxo@gmail.com>
2026-04-02 22:18:30 -0300

    R2 Ex v4.1

--- Roteiro2/Ex/tester.c
+++ Roteiro2/Ex/tester.c
@@ -33,19 +33,52 @@ Tester *constructTester(int N)
 
 void printTree(Tester *test)
 {
+    clock_t start = clock();
     recursive_pre_order_transveral(test->tree, print);
+    clock_t end = clock();
+    double seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nPre-order recursive time: %lf s\n", seconds);
+
     printf("\n\n");
+    
+    start = clock();
     iterative_pre_order_transveral(test->tree, print);
+    end = clock();
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nPre-order iterative time: %lf s\n", seconds);
+
     printf("\n\n\n");
 
+    start = clock();
     recursive_in_order_transveral(test->tree, print);
+    end = clock();
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nIn-order recursive time: %lf s\n", seconds);
+    
     printf("\n\n");
+
+    start = clock();
     iterative_in_order_transveral(test->tree, print);
+    end = clock();
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nIn-order iterative time: %lf s\n", seconds);
+
     printf("\n\n\n");
 
+    start = clock();
     recursive_post_order_transveral(test->tree, print);
+    end = clock();
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nPost-order recursive time: %lf s\n", seconds);
+
     printf("\n\n");
+
+    start = clock();
     iterative_post_order_transveral(test->tree, print);
+    end = clock();
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nPost-order iterative time: %lf s\n", seconds);
+    
     printf("\n\n\n");
 }
 
